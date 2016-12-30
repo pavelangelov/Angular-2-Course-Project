ca7fb70708a6a95d8e6e69b132789518e2b2ccdb
pavelangelov <p_a_v_el@abv.bg>
2016-12-30 22:48:40 +0200

    Started user registration

--- src/app/registration/registration.component.ts
+++ src/app/registration/registration.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { NotificationsService } from 'angular2-notifications';
 
-import { ViewModel} from './registration-model';
+import { ViewModel } from './registration-model';
+import { UserService } from '../user.service';
 
 @Component({
   templateUrl: './registration.component.html',
@@ -8,7 +10,22 @@ import { ViewModel} from './registration-model';
 })
 export class RegistrationComponent implements OnInit {
   private model: ViewModel;
-  constructor() {
+
+  public options = {
+    timeOut: 5000,
+    lastOnBottom: true,
+    clickToClose: true,
+    maxStack: 7,
+    showProgressBar: true,
+    pauseOnHover: true,
+    preventDuplicates: false,
+    preventLastDuplicates: 'visible',
+    rtl: false,
+    animate: 'scale',
+    position: ['right', 'bottom']
+  };
+
+  constructor(private userService: UserService, private _service: NotificationsService) {
     this.model = new ViewModel();
   }
 
@@ -19,8 +36,29 @@ export class RegistrationComponent implements OnInit {
     // validate input parameters
     //    if valid -> send to server and register new user
     //    else -> show error message
-
-    console.log('Input parameters:');
-    console.log(this.model);
+    this.userService.register(this.model)
+      .subscribe(res => {
+        if (res.success) {
+          this._service.success(
+            'Registration',
+            'Completed',
+            {
+              timeOut: 2000,
+              showProgressBar: true,
+              pauseOnHover: false,
+              clickToClose: false
+            });
+        } else {
+          this._service.error(
+            'Registration error',
+            res.error,
+            {
+              timeOut: 3000,
+              showProgressBar: true,
+              pauseOnHover: false,
+              clickToClose: false
+            });
+        }
+      });
   }
 }
