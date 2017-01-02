d80a70246057e7dfa2b1a8e61d9394b290d53556
pavelangelov <p_a_v_el@abv.bg>
2017-01-03 01:23:23 +0200

    Fixes

--- src/app/search-friends/search-friends.component.ts
+++ src/app/search-friends/search-friends.component.ts
@@ -25,7 +25,6 @@ export class SearchFriendsComponent implements OnInit {
             this.notificator.showError('Search error', res.error);
             return;
           }
-
           this.users = res.result;
         });
     } else {
